5277e991fae39e148778d9d6a389e529be4f2c98
VolodymyrPastukh <saske7258@gmail.com>
2021-03-21 12:17:00 +0200

    Auth

--- src/db/Database.kt
+++ src/db/Database.kt
@@ -1,12 +1,26 @@
 package com.lp.db
 
 import com.lp.model.Pack
+import com.mongodb.ConnectionString
+import com.mongodb.MongoClientSettings
+import com.mongodb.MongoCredential
 import org.litote.kmongo.KMongo
 import org.litote.kmongo.getCollection
 
 object Database {
-    private val client = KMongo.createClient("mongodb://database")
-    private val database = client.getDatabase("testDiplom")
+    private val client = KMongo.createClient(
+        MongoClientSettings.builder()
+            .applyConnectionString(ConnectionString("mongodb://database"))
+            .credential(
+                MongoCredential.createCredential(
+                    "campus",
+                    "test",
+                    "detitoorki".toCharArray()
+                )
+            )
+            .build()
+    )
+    private val database = client.getDatabase("test")
     val packs = database.getCollection<Pack>()
 
 
